df053fa4e2ea2975c3fa47d138680ad77192836b
amine <jisoamine@gmail.com>
2023-03-16 15:48:13 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**                                                                                 
- *  *  * Description: main - Prints out a string to stdout.                            
- *   *   * Return: 0 if success.                                                        
- *   */                                                                                  
-int main(void)                                                                      
-{                                                                                   
-	printf("with proper grammar, but the outcome is a piece of art,\n");                
-	return (0);
+
+/**
+ * main - thisis the main function
+ *
+ * Return: 0 when successfull
+ */
+int main(void)                                       
+{                                                                           
+printf("with proper grammar, but the outcome is a piece of art,\n");         
+return (0);
 }
