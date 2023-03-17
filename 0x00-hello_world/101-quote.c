2dc6108d35f9d35f6cf98ab9ea7de986ac567975
amine <jisoamine@gmail.com>
2023-03-16 19:11:21 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
-#include<unistd.h>
-#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ *  * main - print quote
+ *   *
+ *    * Description: prints Dora's quote
+ *     *
+ *      * Return: Always 1 (Success)
+ */
+
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t len = strlen(str);
-	fwrite(str, sizeof(char), len, stdout);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
