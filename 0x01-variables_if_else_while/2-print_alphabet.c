d982525ab5eb07bd599bcb2d94f15504070223aa
amine <jisoamine@gmail.com>
2023-03-17 21:29:54 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * lets play what I call the alphabet game
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+		char a;
+
+		a = 'a';
+		while (a <= 'z')
+	{
+			putchar(a);
+		a++;
+	}
+		putchar(a);
+		return (0);
+}
