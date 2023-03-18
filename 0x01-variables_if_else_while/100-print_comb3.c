75f7a12b263050a960570fc425ce623987db2b31
amine <jisoamine@gmail.com>
2023-03-17 21:19:44 -0700

    1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,8 @@
 /**
  *  * main - Prints comb of numb 00 to 89.
  *   * Return: ALways 0 (Success)
- *    */
+ *
+ */
 int main(void)
 {
 	int a, b;
@@ -11,17 +12,17 @@ int main(void)
 	for (a = 48; a <= 56; a++)
 	{
 		for (b = 49; b <= 57; b++)
-			{ 																if (b > a)
+		{
+			if (b > a)
 				{
 					putchar (a);
 					putchar (b);
 					if (a != 56 || b != 57)
-					{
+						{
 						putchar (',');
 						putchar (' ');
-					}
-				}
-			}
+						}
+		}
 	}
 	putchar('\n');
 	return (0);
