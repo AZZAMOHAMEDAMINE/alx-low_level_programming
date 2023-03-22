3749385479f8c307313000276ce234191ff566c5
amine <jisoamine@gmail.com>
2023-03-22 00:09:26 -0700

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lettre;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+return (0);
+}
